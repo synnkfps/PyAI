82f4897b91852620781e7df524dd4d532f47220f
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-09-28 18:23:05 -0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,18 +1,42 @@
 import random 
 
 inputs = [] # todo: id
-words = []
-outputs = []
+words = {}
+outputs = {}
 topics = []
 pre_words = []
 pro_words = []
 
+questionings = []
+
+def remove_specials(str):
+    return str.replace("'",'').replace('!','')
+
 while True:
     msg = input('You: ')
-    inputs.append(msg)
+    if msg not in inputs:
+        inputs.append(msg)
+
     for i in msg.split():
+        i = remove_specials(i)
+        
         if i not in words:
-            words.append(i)
+            words[i] = 0
+        else:
+            words[i] += 1
+
+        if '?' in i:
+            questionings.append(i)
             
-    print(inputs)
-    print(words)
+    #print(inputs)
+    #print(words)
+    answer = []
+    for i in words:
+        if words[i]>0:
+            answer.append(i)
+    
+    response = ''
+    for i in range(len(answer)):
+        response += random.choice(answer) + ' '
+    
+    print(f'AI: {response}')
